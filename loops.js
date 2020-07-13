5762947cb1a5d9ddbdeb55eb2695bd881d6f0524
Lydia Moore <lydiamoore@Lydias-MacBook-Pro.local>
2020-07-13 16:55:43 -0600

    finished loop practice

---
+++ loops.js
@@ -0,0 +1,19 @@
+for (let i = 0; i < 99; i++){
+    console.log("counting up to 99")
+}
+
+for (let i = 99; i >= 0; i--){
+    console.log("counting down from 99")
+}
+
+for (let i = 0; i < 98; i += 2){
+    console.log("even ascending")
+}
+
+for (let i = 98; i >= 0; i += 2){
+    console.log("even descending")
+}
+
+for (let i = 99; i >= 0; i += 2){
+    console.log("odd descending")
+}
