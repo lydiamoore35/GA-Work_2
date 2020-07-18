6b59e8824760528722f58c4c14f6064a363789d8
Lydia Moore <lydiamoore@Lydias-MacBook-Pro.local>
2020-07-17 18:23:50 -0600

    get even answered

--- unit1/answers_week2.js
+++ unit1/answers_week2.js
@@ -1,7 +1,14 @@
 //////EASY GOING//////
 //I intially had i = 0 but then see that the question asks for a loop logging 1-20 :)
 
-for (let i = 1; i <= 20; i++){
-    console.log(i);
-}
+// for (let i = 1; i <= 20; i++){
+//     console.log(i);
+// }
 
+//////GET EVEN////////
+
+for (let i = 0; i <= 200; i++){
+    if(i % 2 === 0) {
+        console.log(i);
+    }
+}
