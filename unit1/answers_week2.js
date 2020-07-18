684b5f486556fdaa50b60ff09e7ceda116d64684
Lydia Moore <lydiamoore@Lydias-MacBook-Pro.local>
2020-07-17 18:50:33 -0600

    Fizz Buzz answered

--- unit1/answers_week2.js
+++ unit1/answers_week2.js
@@ -7,8 +7,28 @@
 
 //////GET EVEN////////
 
-for (let i = 0; i <= 200; i++){
-    if(i % 2 === 0) {
-        console.log(i);
+// for (let i = 0; i <= 200; i++){
+//     if(i % 2 === 0) {
+//         console.log(i);
+//     }
+// }
+
+//////FIZZ BUZZ/////////
+//////quick question- I tried using 'else if' and then 'else' and it came back with an error that 'else' was an unexpected token, but I don't understand why? 
+
+for (let i = 1; i <= 100; i++){
+    console.log(i)
+}
+for (let i = 1; i <= 100; i++){
+    if(i % 3 === 0) {
+        console.log("Fizz");
+
+    if(i % 5 === 0) {
+        console.log("Buzz");
+
+    if(i % 3 === 0 && i % 5 === 0) {
+        console.log("FizzBuzz");
+    }
+    }
     }
 }
