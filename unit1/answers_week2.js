634c047fe314ad271d93ed9aeb57c058e220f6d3
Lydia Moore <lydiamoore@Lydias-MacBook-Pro.local>
2020-07-19 12:36:10 -0600

    made changes to FIZZ BUZZ

--- unit1/answers_week2.js
+++ unit1/answers_week2.js
@@ -16,19 +16,33 @@
 //////FIZZ BUZZ/////////
 //////quick question- I tried using 'else if' and then 'else' and it came back with an error that 'else' was an unexpected token, but I don't understand why? 
 
+// for (let i = 1; i <= 100; i++){
+//     console.log(i)
+// }
 for (let i = 1; i <= 100; i++){
-    console.log(i)
-}
-for (let i = 1; i <= 100; i++){
+    
     if(i % 3 === 0) {
         console.log("Fizz");
 
-    if(i % 5 === 0) {
+    }else if(i % 5 === 0) {
         console.log("Buzz");
 
-    if(i % 3 === 0 && i % 5 === 0) {
+    }else(i % 3 === 0 && i % 5 === 0);{
         console.log("FizzBuzz");
     }
     }
-    }
-}
+
+
+////EXCITED KITTEN////////
+
+// const kittenResponse = ["...human...why you taking pictures of me?..." , "...the catnip made me do it..." , "...why does the red dot always get away..."]
+// let meow = 0;
+// kittenResponse[meow];
+// for (let i = 0; i <= 20; i++){
+//     if(i % 2 === 0){
+//         return kittenResponse[Math.floor(Math.random()*kittenResponse.length)];
+//         console.log(meow)
+//     }else{console.log("love me, pet me! HSSSSS!")}
+
+// }
+// //////COME BACK TOO
