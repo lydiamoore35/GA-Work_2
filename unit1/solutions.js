a0d3aea795d4d2d6b404650482439db57ea50088
Lydia Moore <lydiamoore@Lydias-MacBook-Pro.local>
2020-07-19 20:38:18 -0600

    sum array completed

--- unit1/solutions.js
+++ unit1/solutions.js
@@ -6,6 +6,8 @@
 
 // There is no output with console.log, while it happended and arguments were passed there is no result. The return statement gives you the value of the function. Alex's butler example in class helped me understand this a bit better. If you ask the bulter to get you oj and they do (console.log) that's great but if they don't bring it to you (return) then you still don't have the result of getting your oj. 
 
+/////Palindrome///////
+
 const checkPalindrome = (string) => {
     const letter = string.split("").reverse().join("").toLowerCase();
     if(string.toLowerCase() === letter){
@@ -17,3 +19,25 @@ const checkPalindrome = (string) => {
 
 console.log(checkPalindrome("Radar"))
 console.log(checkPalindrome("Borscht"))
+
+////////SUM ARRAY////////
+
+const sumArray = (Array) => {
+    let sum = 0;
+    for (let i = 0; i < Array.length; i++){
+       sum += Array[i]; 
+    }
+    return sum;
+};
+console.log(sumArray([1,2,3,4,5,6]));
+
+
+//////PRIME NUMBERS///////
+
+// const checkPrime (num) => {
+//     for (let i = 2; i < num; i++){
+//         if(num % i === 0) {
+//             return false
+//         }else
+//     }
+// }
