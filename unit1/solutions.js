26feb3a80302b135adafc3873aed015f6ea03498
Lydia Moore <lydiamoore@Lydias-MacBook-Pro.local>
2020-07-19 19:15:14 -0600

    palindrome completed

--- unit1/solutions.js
+++ unit1/solutions.js
@@ -5,3 +5,15 @@
 // func(argument);
 
 // There is no output with console.log, while it happended and arguments were passed there is no result. The return statement gives you the value of the function. Alex's butler example in class helped me understand this a bit better. If you ask the bulter to get you oj and they do (console.log) that's great but if they don't bring it to you (return) then you still don't have the result of getting your oj. 
+
+const checkPalindrome = (string) => {
+    const letter = string.split("").reverse().join("").toLowerCase();
+    if(string.toLowerCase() === letter){
+        return true
+    }else{
+        return false
+    }
+    }
+
+console.log(checkPalindrome("Radar"))
+console.log(checkPalindrome("Borscht"))
