509c95065d0c07a78b7f1850221c1eccf9913d58
Lydia Moore <lydiamoore@Lydias-MacBook-Pro.local>
2020-07-25 16:02:12 -0600

    datatype refresher complete

---
+++ unit1/answers_week3.js
@@ -0,0 +1,23 @@
+////light switch////
+=> datatype: Boolean
+=> data structure exmaple: let switch = true
+////user's email address////
+=> datatype: strings in an array
+=> data structure example: const email = ["lydiamoore35@gmail.com"]
+////spaceship////
+=> datatype: strings in an array
+=> data structure example: const spaceship = ["hull", "laser blasters", "tractor beam", "warp drive"]
+////student names////
+=> datatype: strings in an array
+=> data structure example: const studentNames = ["Jennie" , "Heather" , "Bill"]
+////student names and location////
+=> datatype: object with strings as the value for each key (am I using the verbiage correctly?)
+=> data structure example: const studentInfo = {
+                                Jennie: "Denver"
+                                Heather: "Castle Rock"
+                                Bill: "Lonetree"
+////student names, location, and tv show////
+=> datatype: an array that holds multiple objects in the form of strings
+=> data structure example: const studentInfo = [
+        { name: "Jennie" , location: "Denver" , faveTvShow: "Friends"}
+]
---
+++ unit1/landscaper/app.js
@@ -0,0 +1,13 @@
+//using your teeth 
+//you can spend the day cutting lawns and make $1 dollar
+//feel free to do this multiple times 
+//i need to make a wallet with $0 to start 
+
+//this is an object
+const currentTool = {
+    first: "teeth"
+}
+
+//I will use a prompt to ask if you would like to cut grass
+yourAnswer = prompt("Would you like to cut grass for $1?");
+    console.log(yourAnswer)
