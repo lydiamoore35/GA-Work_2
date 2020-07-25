feb23e5b97fe00b9f7c58e0f4ae909502a2acac1
Lydia Moore <lydiamoore@Lydias-MacBook-Pro.local>
2020-07-25 17:26:15 -0600

    crazy object complete

--- unit1/answers_week3.js
+++ unit1/answers_week3.js
@@ -23,13 +23,47 @@
 // ]
 
 ////take it easy////
-const rainbowColors = ["red" , "orange" , "yellow" , "green" , "blue" , "indigo" , "violet"]
-console.log(rainbowColors[4]);
+// const rainbowColors = ["red" , "orange" , "yellow" , "green" , "blue" , "indigo" , "violet"]
+// console.log(rainbowColors[4]);
 
-const LydiaMoore = {
-    food: "pasta",
-    hobby: "yoga",
-    town: "Erie",
-    dataType: "strings"
-}
-console.log(LydiaMoore.hobby)
+// const LydiaMoore = {
+//     food: "pasta",
+//     hobby: "yoga",
+//     town: "Erie",
+//     dataType: "strings"
+// }
+// console.log(LydiaMoore.hobby)
+
+////crazy object////
+const crazyObject = {
+    taco: [{meat: 'steak',
+           cheese: ['panela', 'queso', 'chihuahua']},
+           {meat: 'chicken',
+            salsa: ["pico", "hot", "hotter", "really hot", "really really hot", "omg my mouth is burning"]},
+          ],
+    larry: {
+      nicknames: ["LD", "Chicken Teriyaki Boyyyyyy"],
+      quotes: ["Pretty pretty prettayyyyy good", "Is that a parkinson's thing?", "women love a self confident bald man", "I'm a walking talking enigma"],
+      characters: [{
+        name: "Jeff",
+        occupation: "manager"
+      },
+      {
+        name: "funkhauser",
+        occupation: "tv dude"
+      },
+      {
+        name: "susie",
+        occupation: "jeffs wife",
+        favourtieHobby: "Swearing at Larry and Jeff"
+      },
+      ]
+    }
+  }
+  console.log(crazyObject.taco[1].salsa[5]);//really helpful for me to console.log things one at a time bc i got stuck on this one but i figured it out - wahoo!
+  console.log(crazyObject.larry.quotes[0]);
+  console.log(crazyObject.larry.characters[2].favourtieHobby)
+  console.log(crazyObject.larry.nicknames[1])
+  console.log(crazyObject.larry.characters[1])
+  crazyObject.larry.quotes.push("I'm trying to elevate small talk to medium talk");
+  console.log(crazyObject.larry.quotes)
