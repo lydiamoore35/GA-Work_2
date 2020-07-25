a4e9299d33c7f9e7e0963d741538f27f5bbc5d1f
Lydia Moore <lydiamoore@Lydias-MacBook-Pro.local>
2020-07-25 16:12:30 -0600

    took it easy

--- unit1/answers_week3.js
+++ unit1/answers_week3.js
@@ -1,23 +1,35 @@
-////light switch////
-=> datatype: Boolean
-=> data structure exmaple: let switch = true
-////user's email address////
-=> datatype: strings in an array
-=> data structure example: const email = ["lydiamoore35@gmail.com"]
-////spaceship////
-=> datatype: strings in an array
-=> data structure example: const spaceship = ["hull", "laser blasters", "tractor beam", "warp drive"]
-////student names////
-=> datatype: strings in an array
-=> data structure example: const studentNames = ["Jennie" , "Heather" , "Bill"]
-////student names and location////
-=> datatype: object with strings as the value for each key (am I using the verbiage correctly?)
-=> data structure example: const studentInfo = {
-                                Jennie: "Denver"
-                                Heather: "Castle Rock"
-                                Bill: "Lonetree"
-////student names, location, and tv show////
-=> datatype: an array that holds multiple objects in the form of strings
-=> data structure example: const studentInfo = [
-        { name: "Jennie" , location: "Denver" , faveTvShow: "Friends"}
-]
+// ////light switch////
+// => datatype: Boolean
+// => data structure exmaple: let switch = true
+// ////user's email address////
+// => datatype: strings in an array
+// => data structure example: const email = ["lydiamoore35@gmail.com"]
+// ////spaceship////
+// => datatype: strings in an array
+// => data structure example: const spaceship = ["hull", "laser blasters", "tractor beam", "warp drive"]
+// ////student names////
+// => datatype: strings in an array
+// => data structure example: const studentNames = ["Jennie" , "Heather" , "Bill"]
+// ////student names and location////
+// => datatype: object with strings as the value for each key (am I using the verbiage correctly?)
+// => data structure example: const studentInfo = {
+//                                 Jennie: "Denver"
+//                                 Heather: "Castle Rock"
+//                                 Bill: "Lonetree"
+// ////student names, location, and tv show////
+// => datatype: an array that holds multiple objects in the form of strings
+// => data structure example: const studentInfo = [
+//         { name: "Jennie" , location: "Denver" , faveTvShow: "Friends"}
+// ]
+
+////take it easy////
+const rainbowColors = ["red" , "orange" , "yellow" , "green" , "blue" , "indigo" , "violet"]
+console.log(rainbowColors[4]);
+
+const LydiaMoore = {
+    food: "pasta",
+    hobby: "yoga",
+    town: "Erie",
+    dataType: "strings"
+}
+console.log(LydiaMoore.hobby)
