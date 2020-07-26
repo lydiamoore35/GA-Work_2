fbe08c21062687f6df75c2f98eb24df2077dbb78
Lydia Moore <lydiamoore@Lydias-MacBook-Pro.local>
2020-07-26 14:42:49 -0600

    landscaper - using teeth - work in progress

--- unit1/landscaper/app.js
+++ unit1/landscaper/app.js
@@ -1,11 +1,19 @@
-//using your teeth 
 //you can spend the day cutting lawns and make $1 dollar
+//to do this, you will be using your teeth, initially
 //feel free to do this multiple times 
 //i need to make a wallet with $0 to start 
 
 //this is an object
 const currentTool = {
     first: "teeth"
+}
+const wallet = () => {
+    money = 0;
+    tool = "teeth";
+    askForChoice();
+    }
+const showStatus = () => {
+    alert("You have " + money + " money and ")
 }
 
 //I will use a prompt to ask if you would like to cut grass
@@ -17,4 +25,5 @@ yourAnswer = prompt("Would you like to cut grass for $1?" , "Yes/No");
     }else{
         console.log("Something went wrong")
     }
-
+//you need to add a wallet starting with $0
+//you need to add one dollar if you choose to cut grass with your teeth
