2b89484ff9afc3294d6e7df27d3a0ce53c2b581a
Lydia Moore <lydiamoore@Lydias-MacBook-Pro.local>
2020-07-26 15:39:11 -0600

    landscaper part II

--- unit1/landscaper/app.js
+++ unit1/landscaper/app.js
@@ -3,27 +3,29 @@
 //feel free to do this multiple times 
 //i need to make a wallet with $0 to start 
 
-//this is an object
-const currentTool = {
-    first: "teeth"
-}
-const wallet = () => {
-    money = 0;
-    tool = "teeth";
-    askForChoice();
-    }
-const showStatus = () => {
-    alert("You have " + money + " money and ")
-}
+const currentTool = ["teeth" , "rusty scissors" , "push lawnmower" + "battery-powered lawnmower" + "hire a team"]
 
-//I will use a prompt to ask if you would like to cut grass
+let currentWallet = null;
+let start = () => {
+    alert("You are using " + currentTool + " and have $" + currentWallet);
+}
 yourAnswer = prompt("Would you like to cut grass for $1?" , "Yes/No");
     if(yourAnswer == "Yes"){
-        console.log("Thank you!");
-    }else if (yourAnswer == "No"){
-        console.log("No problem, maybe next time.");
-    }else{
+        prompt("Would you like to use your teeth and earn 1 dollar?" , "Yes/No");
+        if(yourAnswer == "Yes"){
+            currentWallet =+ 1;
+            console.log(start);
+        }else if(yourAnswer == "No"){
+            currentWallet =+ 0
+            console.log("Lazy bones!")
+        }}else{
         console.log("Something went wrong")
     }
+
+
+
+
+
+
 //you need to add a wallet starting with $0
 //you need to add one dollar if you choose to cut grass with your teeth
