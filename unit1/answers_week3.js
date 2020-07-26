6e590d70f56da7f90768dd67081d9bc98102ce4a
Lydia Moore <lydiamoore@Lydias-MacBook-Pro.local>
2020-07-26 10:52:13 -0600

    objection complete

--- unit1/answers_week3.js
+++ unit1/answers_week3.js
@@ -35,35 +35,58 @@
 // console.log(LydiaMoore.hobby)
 
 ////crazy object////
-const crazyObject = {
-    taco: [{meat: 'steak',
-           cheese: ['panela', 'queso', 'chihuahua']},
-           {meat: 'chicken',
-            salsa: ["pico", "hot", "hotter", "really hot", "really really hot", "omg my mouth is burning"]},
-          ],
-    larry: {
-      nicknames: ["LD", "Chicken Teriyaki Boyyyyyy"],
-      quotes: ["Pretty pretty prettayyyyy good", "Is that a parkinson's thing?", "women love a self confident bald man", "I'm a walking talking enigma"],
-      characters: [{
-        name: "Jeff",
-        occupation: "manager"
-      },
-      {
-        name: "funkhauser",
-        occupation: "tv dude"
-      },
-      {
-        name: "susie",
-        occupation: "jeffs wife",
-        favourtieHobby: "Swearing at Larry and Jeff"
-      },
-      ]
+// const crazyObject = {
+//     taco: [{meat: 'steak',
+//            cheese: ['panela', 'queso', 'chihuahua']},
+//            {meat: 'chicken',
+//             salsa: ["pico", "hot", "hotter", "really hot", "really really hot", "omg my mouth is burning"]},
+//           ],
+//     larry: {
+//       nicknames: ["LD", "Chicken Teriyaki Boyyyyyy"],
+//       quotes: ["Pretty pretty prettayyyyy good", "Is that a parkinson's thing?", "women love a self confident bald man", "I'm a walking talking enigma"],
+//       characters: [{
+//         name: "Jeff",
+//         occupation: "manager"
+//       },
+//       {
+//         name: "funkhauser",
+//         occupation: "tv dude"
+//       },
+//       {
+//         name: "susie",
+//         occupation: "jeffs wife",
+//         favourtieHobby: "Swearing at Larry and Jeff"
+//       },
+//       ]
+//     }
+//   }
+//   console.log(crazyObject.taco[1].salsa[5]);//really helpful for me to console.log things one at a time bc i got stuck on this one but i figured it out - wahoo!
+//   console.log(crazyObject.larry.quotes[0]);
+//   console.log(crazyObject.larry.characters[2].favourtieHobby)
+//   console.log(crazyObject.larry.nicknames[1])
+//   console.log(crazyObject.larry.characters[1])
+//   crazyObject.larry.quotes.push("I'm trying to elevate small talk to medium talk");
+//   console.log(crazyObject.larry.quotes)
+
+  ////object-ception////
+
+  const inception = {
+    reality: {
+        dreamLayer1: {
+            dreamLayer2: {
+                dreamLayer3: {
+                    dreamLayer4: {
+                        dreamLayer5: {
+                            dreamLayer6: {
+                                limbo: "Joseph Gordon Levitt"
+                           }
+                        }
+                    }
+                }
+            }
+        }
     }
-  }
-  console.log(crazyObject.taco[1].salsa[5]);//really helpful for me to console.log things one at a time bc i got stuck on this one but i figured it out - wahoo!
-  console.log(crazyObject.larry.quotes[0]);
-  console.log(crazyObject.larry.characters[2].favourtieHobby)
-  console.log(crazyObject.larry.nicknames[1])
-  console.log(crazyObject.larry.characters[1])
-  crazyObject.larry.quotes.push("I'm trying to elevate small talk to medium talk");
-  console.log(crazyObject.larry.quotes)
+ }
+ 
+ inception
+ console.log(inception.reality.dreamLayer1.dreamLayer2.dreamLayer3.dreamLayer4.dreamLayer5.dreamLayer6.limbo = null)
