5694f234e1d0c3baf6da5877063078961b148353
Lydia Moore <lydiamoore@Lydias-MacBook-Pro.local>
2020-07-28 21:36:10 -0600

    I'm am working on changing my if else statements to come before my prompts to seperate user reponses out

--- unit1/landscaper/app.js
+++ unit1/landscaper/app.js
@@ -50,47 +50,72 @@ const cutGrassBy = [
         reward: 250,
     }
 ];
+//user needs to be able to purchase tool
+//need to prompt the user 25 dollar push mower 
+//user needs to be able to landscape and earn money
+//user needs to be able to purchase next tool
+
 let money = 0;
 let wallet = 0;
-const keepCutting = () => {
-    userUpdateScissors = prompt("Would you like to earn " +cutGrassBy[1].reward + " dollars cutting grass with " + cutGrassBy[1].tool + "?" , "Yes/No")
-        if(userUpdateScissors == "Yes" && wallet < 25){
-            wallet += 5;
-            alert("You just earned " + cutGrassBy[1].reward + " dollars! You now have " + wallet + " dollars in your wallet. Keep going!!" );
-        }else if(userUpdateScissors == "No"){
-            alert("Ok, keep cutting grass with your teeth, weirdo.");
+
+const usePushMower = () => {
+    if(money >= 25){
+        purchasePushMower = prompt("Would you like to earn " + cutGrassBy[2].reward + " dollars cutting grass with " + cutGrassBy[2].tool + "?" , "Yes/No")
+        if(purchasePushMower == "Yes" && wallet >= 25){
+            wallet -= 25;
+            alert("You just purchased a " + cutGrassBy[2].tool + " and can now use your new push lawnmower to earn " + cutGrassBy[2].reward + " dollars!!");
+        }else if(purchasePushMower == "No"){
+            alert("LAME!");
+        if(wallet >= 25){
+            userUpdateScissors2 = prompt("Would you like to continue cutting grass with your " + cutGrassBy[2].tool + " and earn another " + cutGrassBy[2].reward + "?" , "Yes/No");
+        }else if(userUpdateScissors2 == "Yes"){
+            wallet += 25;
+            alert("You have " + wallet + " dollars in your wallet. Keep this momentum going, Rockstar!!!");
         }
         keepCutting();
-    }  
-
+    }         
+    }
+}    
+if(wallet < 25){
+    userUpdateScissors = prompt("Would you like to earn " +cutGrassBy[1].reward + " dollars cutting grass with " + cutGrassBy[1].tool + "?" , "Yes/No");{
+    if(userUpdateScissors === "Yes"){
+        wallet += 5;
+        alert("You just earned " + cutGrassBy[1].reward + " dollars! You now have " + wallet + " dollars in your wallet. Keep going!!");
+        keepCutting();
+        if(userUpdateScissors === "No"){
+        alert("I am not worrying about my no's at this point with the mess of recursion I have gotten myself into so, sorry user, you're just going to keep cutting grass.")
+        }
+    }
+    }
+const keepCutting = () => {
+    if(wallet >= 25 && userUpdateScissors === "Yes"){
+        usePushMower();   
+        }
+}    
+   
 const startCutting = () => {
-        //for(i = 0; i < 5; i++){
-            money = cutGrassBy[0].reward;
-            wallet = (wallet + money);
-                startAnswer = prompt("Would you like to earn $1 by cutting grass with your teeth?" , "Yes/No");
-                if(startAnswer == "Yes" && wallet < 5){
-                    alert("You just earned " + cutGrassBy[0].reward + " dollar. Great job! You now have " + wallet + " dollar(s) in your wallet.")
-                    startCutting();
-                }else if(startAnswer == "No"){
-                    alert("Lazy Bones!");
-                    startCutting();
-                }else if(wallet >= 5){
-                     let userUpdate = prompt("Would you like to purchase a pair of " + cutGrassBy[1].tool + "?", "Yes/No");
-            if(userUpdate == "Yes"){
-                wallet -= 5;
-                alert("You have " + wallet + " dollars in your wallet and a lovely pair of rusty scissors, BUT now you can make more $ cutting with scissors!")
-            }else if(userUpdate == "No"){
-                alert("Ok, keep cutting grass with your teeth, weirdo.")
-            } 
-            keepCutting();
-                }else{console.log("Something went wrong")}
-            console.log(money); }
-            
-    
+    money = cutGrassBy[0].reward;
+    wallet = (wallet + money);
+        startAnswer = prompt("Would you like to earn $1 by cutting grass with your teeth?" , "Yes/No");
+        if(startAnswer == "Yes" && wallet < 5){
+            alert("You just earned " + cutGrassBy[0].reward + " dollar. Great job! You now have " + wallet + " dollar(s) in your wallet.")
+            startCutting();
+        }else if(startAnswer == "No"){
+            alert("Lazy Bones!");
+            startCutting();
+        }else if(wallet >= 5){
+                let userUpdate = prompt("Would you like to purchase a pair of " + cutGrassBy[1].tool + "?", "Yes/No");
+        if(userUpdate == "Yes"){
+            wallet -= 5;
+            alert("You have " + wallet + " dollars in your wallet and a lovely pair of rusty scissors, BUT now you can make more $ cutting with scissors!")
+        }else if(userUpdate == "No"){
+            alert("Ok, keep cutting grass with your teeth, weirdo.")
+        } 
+        keepCutting();   
+    } 
+}  
+startCutting();
+}   
     //i need to create another prompt listing the amount of money currently in wallet and then when they have at least $5 to have a follow up prompt asking if they would like to purchase a pair of rusty scissors
     //this needs to be condtioned based on if there is enough money in the wallet 
     //i will write a function that will ask for the user's input
-    // userInput = () => {
-
-    // }
-    startCutting();
