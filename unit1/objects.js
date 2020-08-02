d1a1cb6c1fcc2f708081f530dd6679d4c8c6c1f0
Lydia Moore <lydiamoore@Lydias-MacBook-Pro.local>
2020-08-02 14:30:01 -0600

    created second part of hamster class

--- unit1/objects.js
+++ unit1/objects.js
@@ -1,7 +1,7 @@
 ////Hampster////
 class Hamster {
     constructor (owner, name, price){
-    this.owner = owner;
+    this.owner = name;
     this.name = name;
     this.price = price;
 }
@@ -22,3 +22,53 @@ Pet.eatFood();
 Pet.getPrice();
 
 ////Person////
+class Person {
+    constructor(name, age, height, weight, mood, hamster, bankAccount){
+        this.name = name;
+        this.age = 0;
+        this.height = 0;
+        this.weight = 0;
+        this.mood = 0;
+        this.hamster = [];
+        this.bankAccount = 0;
+    }
+    getName(){
+        return this.name;
+    }
+    getAge(){
+        return this.age;
+    }
+    getWeight(){
+        return this.weight;
+    }
+    greet(){
+        console.log("Amy");
+    }
+    eat(){
+        this.weight++;
+        this.mood++;
+    }
+    exercise(){
+        this.weight--;
+    }
+    ageUp(){
+        this.age++;
+        this.height++;
+        this.weight++;
+        this.mood--;
+        this.bankAccount+10;
+    }
+    buyHamster(hamster){
+        this.hamster.push[Hamster];
+        this.mood++;
+        getPrice(this.price)
+    }
+}
+getName();
+getAge();
+getWeight();
+greet();
+eat();
+exercise();
+ageUp();
+buyHamster();
