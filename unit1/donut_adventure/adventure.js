b4923e1d5e987837d1c54440ab9dcb1880a1a0e0
Lydia Moore <lydiamoore@Lydias-MacBook-Pro.local>
2020-08-02 19:53:44 -0600

    created our enemy pizza rat

--- unit1/donut_adventure/adventure.js
+++ unit1/donut_adventure/adventure.js
@@ -21,5 +21,31 @@ class Hero {
         );
     }
 }
-const dougie = new Hero('Dougie',this.health,this.weapons,this.catchPhrases)
+const dougie = new Hero('Dougie',this.health,this.weapons,this.catchPhrases);
 console.log(dougie);
+
+////Enemy class////
+class Enemy {
+    constructor (name, health, weapons, catchPhrases) {
+        this.name = name;
+        this.health = 100;
+        this.weapons = {
+            pepperoniStars: 5,
+            cheeseGrease: 10,
+        }
+        this.catchPhrases = [
+            'i\'m youtube famous',
+            'i\'m more dangerous than an uncovered sewer']
+    }
+    talkSmack(){
+        console.log(randomNum(this.catchPhrases));
+    }
+    announceHealth(){
+        console.log(this.health);
+    }
+    fight(){
+        console.log('i\'m gonna flatten you like a slice of pepperoni!');
+    }
+}
+const pizzaRat = new Enemy('Pizza Rat',this.health,this.weapons,this.catchPhrases);
+console.log(pizzaRat);
