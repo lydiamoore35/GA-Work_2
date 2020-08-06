d69f8f2081a32afc59ac4828b5cecb3f8370c9a9
Lydia Moore <lydiamoore@Lydias-MacBook-Pro.local>
2020-08-05 20:56:30 -0600

    added attack method to spaceship class

--- unit1/week_5/spacebattle/app.js
+++ unit1/week_5/spacebattle/app.js
@@ -24,10 +24,53 @@
 
 class Spaceship {
     constructor(){
-        this.name = lydiaWynning;
+        this.name = "lydiaWynning";
         this.hull = 20;
         this.firepower = 5;
         this.accuracy = .7;
     }
+//i need to create a method that will attack one of the 6 alien spaceships     
+    attack(alien){
+        if (Math.floor(Math.random() <= alien.accuracy)){
+            console.log("The attack was successful");
+            console.log(enemy.hull -= this.firepower);
+        }else{
+            console.log("You missed" , "Do you want to take another shot?")
+        }
+    }
 }
+const lydiaWynning = new Spaceship();
+console.log(lydiaWynning);
+
 //the methods i will have will be attack, and some method to update the damage done relfecting in the hull points 
+
+class Alien {
+    constructor(name){
+        this.name = name;
+        this.hull = Math.floor(Math.random() * 4) + 3;
+        this.firepower = Math.floor(Math.random() * 3) + 2;
+        this.accuracy = (Math.floor(Math.random() * 3) + 6) / 10;
+    }
+}
+const alien1 = new Alien("alien1");
+//console.log(alien1);
+lydiaWynning.attack(alien1);
+
+const alien2 = new Alien("alien2");
+//console.log(alien2);
+
+const alien3 = new Alien("alien3");
+//console.log(alien3);
+
+const alien4 = new Alien("alien4");
+//console.log(alien4);
+
+const alien5 = new Alien("alien5");
+//console.log(alien5);
+
+const alien6 = new Alien("alien6");
+//console.log(alien6);
+
+//i need to use a method within the classes or a function within a while loop used to establish the act of attacking 
+//attacking needs to factor in the classes methods: hull, firepower, and accuracy, for both lydiaWynning and the 6 alien spaceships 
+    //QUESTION - we want nested loops - one for attacking and another for iterating over aliens??? keep it to one loop??
