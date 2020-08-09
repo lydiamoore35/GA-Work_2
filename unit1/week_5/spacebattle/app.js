a452f1ec9e21cee28cae9eaeedbaa957a26396fb
Lydia Moore <lydiamoore@Lydias-MacBook-Pro.local>
2020-08-09 11:07:12 -0600

    All my files are linked and loaded

--- unit1/week_5/spacebattle/app.js
+++ unit1/week_5/spacebattle/app.js
@@ -21,6 +21,16 @@
 
 //I need to create a class for my spaceship//
     //within the constructor i need name = LydiaWynning, hull = 20, firepower = 5, accuracy = .7
+//const alienFleet = [];
+class Game {
+
+    hullStatus(alien, spaceship){
+       if(this.hull <= 0){
+           const hullStatus = prompt("You successfully hit the spaceship and killed the aliens!")
+       }
+    }   
+   
+    }   
 
 class Spaceship {
     constructor(){
@@ -29,23 +39,29 @@ class Spaceship {
         this.firepower = 5;
         this.accuracy = .7;
     }
-//i need to create a method that will attack one of the 6 alien spaceships 
-//should this be in a for/while loop to continue until the alien's hill reaches 0??    
+ 
     attack(alien){
-        if (Math.floor(Math.random() <= alien.accuracy && alien.hull <= 0)){
-            const intial = prompt("You successfully hit the alien ship! The alien ship has " + (alien.hull -= this.firepower) + " life remaining. Would you like to attack the next alien ship?" , "Yes/No");
-            }else if(alien.hull > 0)
+        const initial = prompt("Earth has been attacked by a horde of aliens! You are the captain of the " + this.name + " on a mission to destroy every last alien ship. Battle the aliens as you try to destroy them with your lasers!" ,"Yes/No");
+        if (Math.floor(((Math.random() * 3) + 6) / 10) <= alien.accuracy && initial === "Yes"){
+            if(alien.hull >= 1){
+            alien1.firepowerStatus();
                 const tryAgain = prompt("Don't let them get away! Would you like to finish them off?" , "Yes/No");
                 if(tryAgain === "Yes"){
-                    alien.attack();
-                }else if(tryAgain === "No"){
+                    lydiaWynning.attack();
+                }else(tryAgain === "No")
                     alert("GAME OVER")
-                }
-        }else{
+                }    
+            }else if(alien.hull <= 0){
+                alert("You successfully destroyed the alien ship!")
+                lydiaWynning.attack(alien2) 
+            } 
+//is there a way to have the next alien ship populate and run in this method and replace alien 2 with alien 3  
+
+        else{
             const nextShot = prompt("You missed! Do you want to take another shot??" , "Yes/No");
                 if(nextShot === "Yes"){
-                    if(Math.floor(Math.random() <= alien.accuracy)){
-                     prompt("You successfully hit the alien ship! The alien ship has " + (alien.hull -= this.firepower) + " life remaining. Would you like to attack the next alien ship?" , "Yes/No")       
+                    if(Math.floor(((Math.random() * 3) + 6) / 10) <= alien.accuracy){
+                     alert("You successfully hit the alien ship! The alien ship has " + (alien.hull -= this.firepower) + " life remaining. Would you like to attack the next alien ship?" , "Yes/No")       
                     }
                 }else(nextShot === "No");{
                     alert("GAME OVER")
@@ -66,6 +82,10 @@ class Alien {
         this.firepower = Math.floor(Math.random() * 3) + 2;
         this.accuracy = (Math.floor(Math.random() * 3) + 6) / 10;
     }
+    firepowerStatus(alien){
+        this.hull -= this.firepower;
+        alert("Your attack was successful. The alien has " + this.hull); 
+        } 
 }
 const alien1 = new Alien("alien1");
 //console.log(alien1);
@@ -86,17 +106,6 @@ const alien5 = new Alien("alien5");
 const alien6 = new Alien("alien6");
 //console.log(alien6);
 
-class Game {
-    constructor(){
-
-    }
-
-    hullStatus(alien, spaceship){
-       if(this.hull <= 0){
-           hullStatus = prompt("You successfully hit the spaceship and killed the aliens!")
-       } 
-    }
-}
 
 //i need to use a method within the classes or a function within a while loop used to establish the act of attacking 
 //attacking needs to factor in the classes methods: hull, firepower, and accuracy, for both lydiaWynning and the 6 alien spaceships 
