3234b9ee995f2732f77b489e51351a299e48891f
Lydia Moore <lydiamoore@Lydias-MacBook-Pro.local>
2020-08-10 16:35:39 -0600

    I added more elements. My power grows with my dedication and hard work

--- unit1/week_5/hogwarts/app.js
+++ unit1/week_5/hogwarts/app.js
@@ -2,7 +2,27 @@
 //     console.log('oops! I still have to link my jQuery properly!');
 //   } else {console.log('I did it! I linked jQuery and this js file!')};
 
-
+////year 1////
 $(() => {
-	// put jQuery in here
+  $('#container') = $container;
+  console.log($container);
+  $('<h1>');
+  $('h1').append('#container');
+  $h1.text("Hogwarts");
 });
+
+////year 2////
+$(() => {
+  $('<h2>');
+  $('h2').append('#container');
+  $h2.text("Lydia Moore");
+  $('<h3>');
+  $('h3').append('#container');
+  $h3.text('Gryffindor');
+  $('<h4>');
+  $('h4').append('#container');
+  $h4.addClass('owl' , 'Hedwig');
+  $h4.text('Hornbeam wand with dragon heartstring core');
+});
+
+////year 3////
