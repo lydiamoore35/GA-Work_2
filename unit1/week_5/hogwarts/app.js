56477dcef2c9bc2f1ddfeccd9fc0bb214879e25e
Lydia Moore <lydiamoore@Lydias-MacBook-Pro.local>
2020-08-11 20:05:03 -0600

    The days go by slowly and the years go by too fast

--- unit1/week_5/hogwarts/app.js
+++ unit1/week_5/hogwarts/app.js
@@ -9,37 +9,32 @@ $(() => {
   const $h1 = $('<h1>');
   $('#container').append($h1);
   $h1.text("Hogwarts");
-});
 
 ////year 2////
-$(() => {
   const $h2 = $('<h2>');
   $h2.appendTo('#container');
   $h2.text("Lydia Moore");
   const $h3 = $('<h3>');
   $h3.appendTo('#container');
   $h3.text('Gryffindor');
-  const $h4 = $('<h4>');
+  const $h4 = $('<h4>Hedwig</h4>').addClass('Owl');
   $h4.appendTo('#container');
-  $h4.addClass('owl' , 'Hedwig');
-  $h4.text('Hornbeam wand with dragon heartstring core');
-});
+  const $h4Two = $('<h4>Hornbeam wand with dragon heartstring core</h4>');
+  $h4Two.appendTo('#container');
 
 ////year 3////
-$(() => {
   const $list = $('<ul></ul>').attr('storage' , 'trunk');
-  $('<li>butter beer</li>').appendTo($list);
+  const $beer = $('<li>butter beer</li>').appendTo($list);
   const $cloak = $('<li>invisibility cloak</li>')
   $cloak.appendTo($list);
   $cloak.addClass('secret');
   $('<li>magic map</li>').appendTo($list).addClass('secret');
   $('<li>time turner</li>').appendTo($list).addClass('secret');
+  $('<li>leash</li>').appendTo($list).addClass('Owl');
   $("<li>Bertie Bott's Every Flavor Jelly Beans</li>").appendTo($list).addClass('secret');
   $list.appendTo('#container');
-});
 
 ////year 4////
-$(() => {
   const $h5 = $('<h5>');
   const $table = $('<table>');
   $table.appendTo('#container');
@@ -61,19 +56,19 @@ $(() => {
   const $tr5 = $('<tr>').appendTo($thead);
   $('<td>').text('Friday').appendTo($tr5);
   $('<td>').text('class 10, class 11, class 12').appendTo($tr5);
-});
 
 ////year 5////
-$(() => {
-
-});
-
+  $h4Two.remove();
+  $beer.remove();
+  const $h4NewWand = $('<h4>This new wand is super awesome</h4>').appendTo('#container');
+  $h4NewWand.css('color', 'pink');
+  $h4.hide();
+  $h4.show();
+  
 ////year 6////
-$(() => {
 
-});
 
 ////year 7////
-$(() => {
+
 
 });
