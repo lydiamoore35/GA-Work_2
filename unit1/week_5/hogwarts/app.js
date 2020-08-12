c5e1a8d4795bcc832885173893af1a6427b8b0ae
Lydia Moore <lydiamoore@Lydias-MacBook-Pro.local>
2020-08-11 18:45:03 -0600

    I realize I can make things that are ever increasingly more complex with greater ease. Adding order is always more effort than causing chaos

--- unit1/week_5/hogwarts/app.js
+++ unit1/week_5/hogwarts/app.js
@@ -36,21 +36,31 @@ $(() => {
   $('<li>time turner</li>').appendTo($list).addClass('secret');
   $("<li>Bertie Bott's Every Flavor Jelly Beans</li>").appendTo($list).addClass('secret');
   $list.appendTo('#container');
-  
-
 });
 
 ////year 4////
 $(() => {
   const $h5 = $('<h5>');
   const $table = $('<table>');
-  const $tr = $('<tr>');
-  $table.append('<tr></tr>');
-  $h5.appendTo('#container');
   $table.appendTo('#container');
-  const $th = $('<th></th>');
-  $tr.append('<th></th>');//can't seem to get the th to appear within the tr within the table??
-  
+  const $thead = $('<thead>').appendTo($table);
+  $('<th>').text('Day').appendTo($thead);
+  $('<th>').text('Classes').appendTo($thead);
+  const $tr = $('<tr>').appendTo($thead);
+  $('<td>').text('Monday').appendTo($tr);
+  $('<td>').text('Herbology, Divination and History of Magic').appendTo($tr);
+  const $tr2 = $('<tr>').appendTo($thead);
+  $('<td>').text('Tuesday').appendTo($tr2);
+  $('<td>').text('class 1, class 2, class 3').appendTo($tr2);
+  const $tr3 = $('<tr>').appendTo($thead);
+  $('<td>').text('Wednesday').appendTo($tr3);
+  $('<td>').text('class 4, class 5, class 6').appendTo($tr3);
+  const $tr4 = $('<tr>').appendTo($thead);
+  $('<td>').text('Thursday').appendTo($tr4);
+  $('<td>').text('class 7, class 8, class 9').appendTo($tr4);
+  const $tr5 = $('<tr>').appendTo($thead);
+  $('<td>').text('Friday').appendTo($tr5);
+  $('<td>').text('class 10, class 11, class 12').appendTo($tr5);
 });
 
 ////year 5////
