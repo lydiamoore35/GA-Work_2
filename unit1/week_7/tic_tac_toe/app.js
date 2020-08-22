3b8c71267941b0b2e130b5fc23da7f961c592c97
Lydia Moore <lydiamoore@Lydias-MacBook-Pro.local>
2020-08-21 18:08:35 -0600

    completed tic tac toe. tried to use this but went back to currenttarget and finished mvp

--- unit1/week_7/tic_tac_toe/app.js
+++ unit1/week_7/tic_tac_toe/app.js
@@ -1,16 +1,22 @@
-console.log('jquery is set up')
+//console.log('jquery is set up')
 
-$(() => {
+let player = 1;
 
+//$(() => {
+    
 $('.square').on('click' , (event) => {
-    $(event.currentTarget).text('X');
-
+    //let choice = $(this);
+        if($(event.currentTarget).hasClass('X') || ($(event.currentTarget)).hasClass('O')) {
+            alert('This square has already been chosen. Please pick an empty square.')
+        }else{
+            if(player === 1) {
+                $(event.currentTarget).addClass('X').text('X');
+                player = 2;
+            }else{
+                $(event.currentTarget).addClass('O').text('O');
+                player = 1;
+            }
+        }     
 });
 
-$('.square').on('click' , (event) => {
-    $(event.currentTarget).text('Y');
-
-});
-
-
-});
+//});
