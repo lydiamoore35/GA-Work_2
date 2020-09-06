9dc97433c3d08ba8830c081e8891fbe2a3e438ae
Lydia Moore <lydiamoore@Lydias-MacBook-Pro.local>
2020-09-06 14:54:03 -0600

    Tip Calculator express application created, not quite working

--- unit2/week9_HW_express.js/server.js
+++ unit2/week9_HW_express.js/server.js
@@ -6,6 +6,15 @@ app.get('/greeting/:name/:name2/:name3', (req, res) => {
     console.log(req.params);
     res.send(`Hello ${req.params.name} and ${req.params.name2} and ${req.params.name3}`)
 })
+
+app.get('/tip/:total/:tipPercentage', (req, res) => {
+    let amount = req.params.total;
+    let tipPercentage = req.params. tipPercentage;
+    let tipTotal = eval((tipPercentage/100) * amount)
+    console.log(tipTotal); 
+    res.send(tipTotal);
+});
+
 app.listen(PORT, () => {
     console.log('testing');
-})
+});
