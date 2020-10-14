4a1606741b9f99e0adfea55efbd743acb7999d21
Lydia Moore <lydiamoore@Lydias-MacBook-Pro.local>
2020-10-13 22:22:28 -0600

    tardis is DONE and the divs update independently

--- unit2/week_14_tardis/tardis/src/components/divThree.js
+++ unit2/week_14_tardis/tardis/src/components/divThree.js
@@ -1,6 +1,30 @@
 import React from "react"
 
 const DivThree = (props) => {
+    const [state, setState] = React.useState({
+        tardis:{  
+           name2: "Time and Relative Dimension in Space",
+           caps2: false
+      } }) 
+    
+      const changeIt2 = (text) => {
+        if (state.tardis.caps2) {
+          setState({
+            tardis: {
+              name2: text.toLowerCase(),
+              caps2: false
+            }
+          })
+        } else {
+          setState({
+            tardis: {
+              name2: text.toUpperCase(),
+              caps2: true
+            }
+          })
+        }
+      }
+
     const {name, change} = props;
 
     return (
@@ -9,7 +33,7 @@ const DivThree = (props) => {
             <h3 onClick={() => change(name)}>{name}</h3>
         </div>
         <div>
-            <h3 onClick={() => change(name)}>{name}</h3>
+            <h3 onClick={() => changeIt2(state.tardis.name2)}>{state.tardis.name2}</h3>
         </div>
         </>
     )
