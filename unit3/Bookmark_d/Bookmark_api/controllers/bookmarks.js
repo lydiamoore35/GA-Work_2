7e450c607788c0de2e77e9463612bdb5844d3115
Lydia Moore <lydiamoore@Lydias-MacBook-Pro.local>
2020-10-20 19:49:22 -0600

    testing to enterprise

--- unit3/Bookmark_d/Bookmark_api/controllers/bookmarks.js
+++ unit3/Bookmark_d/Bookmark_api/controllers/bookmarks.js
@@ -2,54 +2,53 @@ const express = require('express')
 const bookmarks = express.Router()
 const Bookmark = require('../models/bookmark.js')
 
-// SEED route
+////// Routes //////
+
+// SEED
 bookmarks.get('/seed', (req, res) => {
-    Bookmark.create({title: 'Hello'}, (err, createdBookmark) => {
+    Bookmark.create({title: 'Hello, the seed route is working'}, (err, createdBookmark) => {
         res.json(createdBookmark)
     })
 })
 
 // INDEX
+bookmarks.get('/', (req, res) => {
+  Bookmark.find(req.body, (error, foundBookmark) => {
+    if (error) {
+      res.status(400).json({ error: error.message })
+    }
+    res.status(200).json(foundBookmark)
+  })
+})
+
+// DELETE
+bookmarks.delete('/:id', (req, res) => {
+    Bookmark.findByIdAndRemove(req.params.id, (err, deletedBookmark) => {
+      if (err) {
+        res.status(400).json({ error: err.message })
+      }
+      res.status(200).json(deletedBookmark)
+    })
+  })
+
+// UPDATE
+bookmarks.put('/:id', (req, res) => {
+    Bookmark.findByIdAndUpdate(req.params.id, req.body, { new: true }, (err, updatedBookmark) => {
+      if (err) {
+        res.status(400).json({ error: err.message })
+      }
+      res.status(200).json(updatedBookmark)
+    })
+  })
+
+// CREATE
 bookmarks.post('/', (req, res) => {
   Bookmark.create(req.body, (error, createdBookmark) => {
     if (error) {
       res.status(400).json({ error: error.message })
     }
-    res.status(200).json(createdBookmark) //  .json() will send proper headers in response so client knows it's json coming back
-  })
+    res.status(200).json(createdBookmark)
+   })
 })
-
-// // NEW - we don't do this with an API
-
-// // DELETE
-// holidays.delete('/:id', (req, res) => {
-//     Holiday.findByIdAndRemove(req.params.id, (err, deletedHoliday) => {
-//       if (err) {
-//         res.status(400).json({ error: err.message })
-//       }
-//       res.status(200).json(deletedHoliday)
-//     })
-//   })
-
-// // UPDATE
-// holidays.put('/:id', (req, res) => {
-//     Holiday.findByIdAndUpdate(req.params.id, req.body, { new: true }, (err, updatedHoliday) => {
-//       if (err) {
-//         res.status(400).json({ error: err.message })
-//       }
-//       res.status(200).json(updatedHoliday)
-//     })
-//   })
-
-// // CREATE
-// holidays.post('/', (req, res) => {
-//   Holiday.create(req.body, (error, createdHoliday) => {
-//     if (error) {
-//       res.status(400).json({ error: error.message })
-//     }
-//     res.status(200).json(createdHoliday) //  .json() will send proper headers in response so client knows it's json coming back
-//    })
-// })
-
 
 module.exports = bookmarks
