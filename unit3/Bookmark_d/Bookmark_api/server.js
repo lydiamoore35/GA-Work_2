903a8483f3e46df1961c6c9ad316c8b6f6a00791
Lydia Moore <lydiamoore@Lydias-MacBook-Pro.local>
2020-10-23 19:37:37 -0600

    testing lame

--- unit3/Bookmark_d/Bookmark_api/server.js
+++ unit3/Bookmark_d/Bookmark_api/server.js
@@ -1,12 +1,15 @@
 const express = require('express')
 const app = express()
-const PORT = 3003
+const PORT = 5500
 const bookmarksController = require('./controllers/bookmarks.js')
 const mongoose = require('mongoose')
+const cors = require("cors");
 
 // Error 
 mongoose.connection.on('error', err => console.log(err.message + ' is Mongod not running?'))
 mongoose.connection.on('disconnected', () => console.log('mongo disconnected'))
+
+app.use(cors());
 
 // Middleware
 app.use(express.json()); 
