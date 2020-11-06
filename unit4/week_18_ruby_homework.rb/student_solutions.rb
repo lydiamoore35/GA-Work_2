af981b9b7f05d7b616b7b143256072f92e4ee4a7
Lydia Moore <lydiamoore35@gmail.com>
2020-11-06 16:20:16 -0700

    color array complete

--- unit4/week_18_ruby_homework.rb/student_solutions.rb
+++ unit4/week_18_ruby_homework.rb/student_solutions.rb
@@ -5,6 +5,10 @@
 # end
 # hello("big bad")
 
+
+###### Nums Array and Enumerables ######
+
+
 # def array
 #     nums = [5, 5, 6, 7, 2, 4, 3, 2, 1, 9, 7, 6, 0, 0, 35, 19, 66, 3, 22, 21, 4, 4, 4, 0, 1, 2, 3, 4, 12, 15]
 #     puts nums.uniq
@@ -143,3 +147,25 @@
 # end
 
 # array()
+
+
+###### Color Array ######
+
+
+def color_array
+    colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
+    puts colors.sample()
+end
+color_array()
+
+def color_array
+    colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
+    puts colors.reverse()
+end
+color_array()
+
+def color_array
+    colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
+    colors.each_with_index { |x| puts x.upcase}
+end
+color_array()
