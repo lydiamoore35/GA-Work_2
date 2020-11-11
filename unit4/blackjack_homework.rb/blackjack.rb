20d379685b66ac8c6c2225d06443defffb1a367e
Lydia Moore <lydiamoore35@gmail.com>
2020-11-10 20:19:02 -0700

    creating card logic not working and needs to be revisited

--- unit4/blackjack_homework.rb/blackjack.rb
+++ unit4/blackjack_homework.rb/blackjack.rb
@@ -16,10 +16,11 @@ puts human.bankroll
 puts computer
 
 class Card
-    attr_accessor :value, suit
+    attr_accessor :value, :suit, :face
     def initialize
         @value = value
         @suit = suit
+        @face = face
     end
 end
 
@@ -28,3 +29,19 @@ class Deck
         @faces = [*(2..10), 'Jack', 'Queen', 'King', 'Ace']
         @suits = ['clubs', 'spades', 'hearts', 'diamonds']
         @cards = []
+
+#### create deck of cards ####
+@faces.each do |face|
+    if face.class == Integer
+        value = face
+    elsif face == 'Ace'
+        value = 11
+    else
+        value = 10
+    end
+    @suits.each do |suit|
+        @cards << Card.new(value, suit, face) 
+    end
+end
+@cards.shuffle!
+end       
