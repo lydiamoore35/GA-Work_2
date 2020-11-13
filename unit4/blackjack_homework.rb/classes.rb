98ef0ff6d94fc8b6ef15e7033d18d18a936585fc
Lydia Moore <lydiamoore35@gmail.com>
2020-11-12 17:24:08 -0700

    lots or refactoring and now only 2 classes and all other logic is in main

--- unit4/blackjack_homework.rb/classes.rb
+++ unit4/blackjack_homework.rb/classes.rb
@@ -14,58 +14,16 @@ class Card
         @value = value
     end
     def value
+        puts value
     end
 end
 
-class Deck
-    attr_accessor :cards, :faces
-    def initialize 
-        @faces = [*(2..10), 'Jack', 'Queen', 'King', 'Ace']. each do |face|
-            cards << Card.new(face)
-        @cards = []
-        cards.shuffle!
-    end
-    deck = Deck.new
-    hand = deck.draw(2)
-end
-end
 
 
 
 
 
 
-# # #------------------
-# # # PLAYER class
-# class PLAYER
-#     attr_accessor :name, :hand, :bankroll
-#     def initialize name, bankroll
-#         @name = name
-#         @hand = []
-#         @bankroll= bankroll
-#     end 
-# end 
-# #----------------
-# DECK class: create a radom deck of 53 playing cards 
-# class DECK
-#     attr_accessor :cards
-#     def initialize 
-#         @cards = []
-#         [:clubs, :diamonds, :spades, :hearts].each do |suit|
-#             (2..10).each do |number|
-#                 cards << CARD.new(suit, number)
-#             end 
-#             ["J", "Q", "K", "A"].each do |facecard|
-#                 cards << CARD.new(suit, facecard)
-#             end
-#         end
-#         # Shuffles elements in self in place.
-#         cards.shuffle!
-#     end
-#     def deal
-#         cards.pop(2)
-#     end
-# end
 #---------------
 # CARD class
 # class CARD
--- unit4/blackjack_homework.rb/main.rb
+++ unit4/blackjack_homework.rb/main.rb
@@ -1,5 +1,18 @@
 require "./classes.rb"
 
+Human = Player.new "name", 100, []
+Computer = Player.new "Computer", 100000, []
+
+def make_deck
+    @faces = [*(2..10), 'Jack', 'Queen', 'King', 'Ace'].each do |face|
+        cards << Card.new(face)
+    @cards = []
+    cards.shuffle!
+    human = human.hand
+    hand = deck.pop(2)
+    end
+end
+
 puts "what is your name?"
 name = gets.chomp
 puts "Do you want to play blackjack? [y]es or [n]o"
@@ -11,27 +24,26 @@ if input == "yes"
     puts "Let's begin!"
 end
 
+make_deck()
 
-while true do
-    puts "Your bankroll amount: #{human.bankroll}" && "Comupter's bankroll amount: #{computer.bankroll}" > 0
-    puts "Your cards are: "
-    puts hand
-    value = hand.map(&:value).get_sum
-    puts "Your hand's value is #{value}"
-    if value > 21
-        puts "You lose, sorry!"
-        break
-    end
-    puts "[h]it or [s]tay?"
+# while true do
+#     puts "Your bankroll amount: #{Human.bankroll}" && "Computer's bankroll amount: #{Computer.bankroll}" > 0
+#     puts "Your cards are: "
+#     puts hand
+#     value = hand.map(&:value).get_sum
+#     puts "Your hand's value is #{value}"
+#     if value > 21
+#         puts "You lose, sorry!"
+#         break
+#     end
+#     puts "[h]it or [s]tay?"
     
-end
-
+# end
 
 # puts human.name
 # puts human.bankroll
 # puts computer
-Human = Player.new name, 100, []
-Computer = Player.new "Computer", 100000, []
+
 
 # while true do
 #     puts "Humans's bankroll amount: #{human.bankroll} - #{Player.name}'s Life: #{Player.health}"
@@ -58,4 +70,3 @@ Computer = Player.new "Computer", 100000, []
 #         puts "Cleetus has been fatally wounded, you win!"
 #         break
 #     end 
-# end
