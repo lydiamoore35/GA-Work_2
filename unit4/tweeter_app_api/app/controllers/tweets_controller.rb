59e4e5d04817d6a2573ba2c8162cac25820eb9e1
Lydia Moore <lydiamoore35@gmail.com>
2020-11-24 21:31:05 -0700

    Can POST new Tweets

--- unit4/tweeter_app_api/app/controllers/tweets_controller.rb
+++ unit4/tweeter_app_api/app/controllers/tweets_controller.rb
@@ -9,4 +9,20 @@ class TweetsController < ApplicationController
         render(json:{tweet:tweet})
     end
 
+    def create
+        tweet = Tweet.new(params[:id])
+
+    if tweet.save
+        render json:{tweet: tweet}
+    else
+        render(status: 422, json: { tweet: tweet, errors: tweet.errors})
+    end
+    end
+
+end
+
+private
+
+def item_params
+  params.require(:item).permit(:name, :price)
 end
